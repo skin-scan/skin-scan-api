e9b2cfea2ddb6b03cd41d7aac5941bdf6290ca20
AnggaPutraa <yananggaa@gmail.com>
2024-05-23 15:57:44 +0700

    refactor(prisma): remove redundant migration

--- prisma/migrations/20240523085643_init_table_creation/migration.sql
+++ prisma/migrations/20240523085643_init_table_creation/migration.sql
@@ -1,18 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `detections` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "detections" DROP CONSTRAINT "detections_user_id_fkey";
-
--- DropTable
-DROP TABLE "detections";
-
--- DropTable
-DROP TABLE "users";
+-- CreateEnum
+CREATE TYPE "DetectionStatus" AS ENUM ('DIAGNOSED', 'SAFE');
 
 -- CreateTable
 CREATE TABLE "User" (
